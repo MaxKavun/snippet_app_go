4eff266b7b21f7d4e6428bd121c6dc0d187e5d3c
max <maxkavun@outlook.com>
2021-11-09 08:34:43 +0300

    Add initial code for snippet app with working DB connection, quering list of snippets in DB, add a new snippet via form, handling get\post requests via special router

---
+++ cmd/web/handlers.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"errors"
+	"maxkavun.ml/snippetbox/pkg/models"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {	
+	s, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: s,
+	})
+
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
+}
+
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "create.page.tmpl", nil)
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires := r.PostForm.Get("expires")
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
+}
---
+++ cmd/web/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"html/template"
+	"flag"
+	"os"
+	"maxkavun.ml/snippetbox/pkg/models/mysql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+	snippets *mysql.SnippetModel
+	templateCache map[string]*template.Template
+}
+
+func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP Network address")
+	dsn := flag.String("dsn", "root:qwerty@/snippetbox?parseTime=true", "MySQL data source name")
+
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	db.SetMaxOpenConns(5)
+	db.SetMaxIdleConns(5)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	templateCache, err := newTemplateCache("./ui/html")
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
+		templateCache: templateCache,
+	}
+
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+
+}
